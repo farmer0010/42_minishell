eff470c6e82f3c6d4f77133291902e96addb2b58
Juyoung Kim <juyoukim@c3r10s6.42gyeongsan.kr>
2025-07-09 13:46:48 +0900

    feat: Uploading a header temporary file

---
+++ includes/minishell.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juyoukim <juyoukim@student.42gyeongsa      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/09 13:44:33 by juyoukim          #+#    #+#             */
+/*   Updated: 2025/07/09 13:44:43 by juyoukim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../Libft/libft.h"
+
+#endif
