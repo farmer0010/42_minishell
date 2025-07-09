0da6b46aedee5ce7a957e411c1985963648e6ba3
Juyoung Kim <juyoukim@c3r10s6.42gyeongsan.kr>
2025-07-09 13:54:16 +0900

    feat: add base header file (minishell.h)

--- includes/minishell.h
+++ includes/minishell.h
@@ -27,4 +27,22 @@
 # include <readline/history.h>
 # include "../Libft/libft.h"
 
+# define READ_END 0
+# define WRITE_END 1
+# define TRUE 1
+# define FALSE 0
+
+typedef struct s_cmd {
+	char **argv;
+	int infile;
+	int outfile;
+	int is_builtin;
+	struct s_cmd *next;
+} t_cmd;
+
+void	execute_cmds(t_cmd *cmd);
+t_cmd	*make_fake_cmd(void);
+void	free_all(t_cmd *cmd, char *input);
+void	free_argv(char **argv);
+
 #endif
