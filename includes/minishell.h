2837eb81b2b9d4160cf60b97f19f70c74ba9b583
Taewon Kim <taewonki@c3r10s5.42gyeongsan.kr>
2025-07-10 16:46:46 +0900

    파싱 관련 구조체 헤더 추가 + parse-tokenize 이동 전 커밋

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juyoukim <juyoukim@student.42gyeongsa      +#+  +:+       +#+        */
+/*   By: taewonki <taewonki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 13:44:33 by juyoukim          #+#    #+#             */
-/*   Updated: 2025/07/09 13:44:43 by juyoukim         ###   ########.fr       */
+/*   Updated: 2025/07/10 16:33:27 by taewonki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,48 @@ typedef struct s_cmd {
     struct s_cmd *next;
 } t_cmd;
 
+typedef struct s_statemachine
+{
+    int s_in_general;
+    int s_in_word;
+    int s_in_single_quote;
+    int s_in_double_quote;
+    int s_in_operation;
+}   t_statemachine;
+
+typedef enum e_token_type
+{
+    token_word,
+    // 일반적인 단어 ls, cat, $HOME 등의 "따옴표 안의 내용"
+    token_pipe,
+    // 파이프 ( | )
+    token_redirect_out,
+    // 출력 리다이렉션 ( > )
+    token_append_redirect,
+    // 추가 출력 리다이렉션 ( >> )
+    token_redirect_in,
+    // 입력 리다이렉션 ( < )
+    token_redirect_here_doc
+    // 히어독 ( << )
+}   t_token_type;
+
+typedef struct  s_token
+{
+    int token_type;
+    // 토큰 종류
+    char    *value;
+    // 토큰의 실제 문자열 값
+}   t_token;
+
 void    execute_cmds(t_cmd *cmd);
 char    *find_executable(char *cmd_name, char **envp);
 char    *ft_strjoin_three(const char *s1, const char *s2, const char *s3);
 void    free_argv(char **argv);
+
+//parse_util.c
+int	ft_isspace(char c);
+
+//lexing.c
+int lexing(char *cmd);
 
 #endif
