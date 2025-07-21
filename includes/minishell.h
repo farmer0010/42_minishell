4445752c832c5d92bb7e3d0c47463b2c84777dcd
Juyoung Kim <juyoukim@c1r20s7.42gyeongsan.kr>
2025-07-21 12:21:03 +0900

    feat: 헤더파일 .c파일들마다 정리 및 파이프 구조체, 쉘 정보 구조체 추가

--- includes/minishell.h
+++ includes/minishell.h
@@ -5,11 +5,10 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: taewonki <taewonki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: Invalid date        by                   #+#    #+#             */
+/*   Created: 2025/07/16 14:13:30 by taewonki          #+#    #+#             */
 /*   Updated: 2025/07/16 14:13:30 by taewonki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -22,6 +21,7 @@
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <limits.h>
+# include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "libft.h"
@@ -34,68 +34,172 @@
 #  define FALSE 0
 # endif
 
-typedef struct s_cmd {
-    char **argv;
-    int infile;
-    int outfile;
-    int is_builtin;
-    struct s_cmd *next;
-} t_cmd;
+extern int	g_exit_status;
 
-typedef enum e_state {
-    s_in_general,
-    // 일반적인 상태, 따옴표, 특수 연산자 바깥에 있는 상태
-    s_in_double_quote,
-    // 더블 쿼트 안에 있는 상태, / $ 등의 문자는 처리되어야 한다.
-    s_in_single_quote,
-    // 싱글 쿼트 안에 있는 상태, 내부의 모든 문자는 리터럴로 처리되어야 한다.
-    s_in_word
-    // 단어 안에 있는 상태
-} t_state;
+/*
+** t_env: 환경 변수의 key-value 쌍
+*/
+typedef struct s_env
+{
+	char	*key;
+	char	*value;
+}	t_env;
 
-typedef enum e_token_type {
-    WORD,
-    // 일반 단어, 명령어도 여기 포함
-    OPERATOR,
-    PIPE,
-    REDIRECT_IN,
-    HERE_DOC,
-    REDIRECT_OUT,
-    REDIRECT_APPEND
-} t_token_type;
+/*
+** t_cmd: 파싱된 명령어 정보를 담는 구조체
+*/
+typedef struct s_cmd
+{
+	char			**argv;
+	int				infile;
+	int				outfile;
+	struct s_cmd	*next;
+}	t_cmd;
 
-typedef struct s_token {
-    char *value;
-    t_token_type type;
-} t_token;
-// 여기서 lexing이 완료되어서 토큰화 되어서 이 구조체에 저장된 정보는
-// 원자적인 최소단위 정보이다.
+/*
+** t_state: 렉서가 문자열을 토큰화할 때의 상태
+*/
+typedef enum e_state
+{
+	s_in_general,
+	s_in_double_quote,
+	s_in_single_quote,
+	s_in_word
+}	t_state;
 
-typedef struct s_node {
-    t_token *token;
-    struct s_node *prev;
-    struct s_node *next;
-} t_node;
+/*
+** t_token_type: 토큰의 종류
+*/
+typedef enum e_token_type
+{
+	WORD,
+	OPERATOR,
+	PIPE,
+	REDIRECT_IN,
+	HERE_DOC,
+	REDIRECT_OUT,
+	REDIRECT_APPEND
+}	t_token_type;
 
-// lexing.c
+/*
+** t_token: 렉싱 결과 생성된 개별 토큰
+*/
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+}	t_token;
 
+/*
+** t_node: 토큰 리스트 노드
+*/
+typedef struct s_node
+{
+	t_token			*token;
+	struct s_node	*prev;
+	struct s_node	*next;
+}	t_node;
 
-void    execute_cmds(t_cmd *cmd);
-char    *find_executable(char *cmd_name, char **envp);
-char    *ft_strjoin_three(const char *s1, const char *s2, const char *s3);
-int		handle_redirects(t_cmd *cmd);
-int		ft_strcmp(const char *s1, const char *s2);
-void    free_argv(char **argv);
+/*
+** t_pipe_data: 파이프 실행 관련 데이터
+*/
+typedef struct s_pipe_data
+{
+	int		idx;
+	int		count;
+	int		**pipefd;
+}	t_pipe_data;
 
-// parse_utils.c
-int		ft_isquote(char c);
-int		ft_isspace(char c);
-char	handle_escape(char c);
-int		ft_isoper(char c);
-int		ft_isspace(char c);
+/*
+** t_shell_data: 쉘의 전체 상태 정보
+*/
+typedef struct s_shell_data
+{
+	t_list			*env_list;
+	t_cmd			*cmd_list;
+	t_node			*token_list;
+	t_pipe_data		pipe_data;
+	int				stdin_backup;
+	int				stdout_backup;
+}	t_shell_data;
 
-// list_func.c
-t_node	*create_node(int type, char *value);
-void	append_token(t_node **head, t_node *node);
+/* execute_cmds.c */
+void		execute_cmds(t_shell_data *data);
+
+/* execute_cmds_utils.c */
+char		*find_executable(char *cmd_name, t_list *env_list);
+char		*ft_strjoin_three(const char *s1, const char *s2, const char *s3);
+
+/* redirect.c */
+int			handle_redirects(t_cmd *cmd);
+
+/* libft_plus_utils.c */
+int			ft_strcmp(const char *s1, const char *s2);
+int			is_numeric(const char *str);
+
+/* free_utils.c */
+void		free_argv(char **argv);
+void		free_all(t_shell_data *data);
+void		free_cmd_list(t_cmd *cmd_list);
+void		free_token_list(t_node *token_list);
+void		free_env_node(void *content);
+
+/* signal_utils.c */
+void		sigint_handler(int signo);
+void		sigquit_handler(int signo);
+
+/* signal.c */
+void		init_signal(void);
+
+/* builtin.c */
+int			is_builtin(char *cmd);
+int			exec_builtin(t_cmd *cmd, t_shell_data *data);
+
+/* builtin_utils.c */
+int			builtin_echo(char **argv);
+int			builtin_cd(char **argv, t_list *env_list);
+int			builtin_pwd(void);
+int			builtin_env(t_list *env_list);
+int			builtin_exit(char **argv, t_shell_data *data);
+
+/* builtin_utils2.c (export) */
+int			builtin_export(char **argv, t_list **env_list);
+int			is_valid_identifier(const char *str);
+
+/* builtin_utils3.c (unset) */
+int			builtin_unset(char **argv, t_list **env_list);
+
+/* env_manage.c */
+t_list		*init_env_list(char **envp_sys);
+char		*get_env_value(t_list *env_list, const char *key);
+int			set_env_value(t_list **env_list, const char *key, const char *value);
+int			unset_env_value(t_list **env_list, const char *key);
+char		**convert_env_list_to_array(t_list *env_list);
+void		free_env_array(char **env_array);
+
+/* pipe_utils.c */
+int			**create_pipes(int count);
+void		setup_pipes(int idx, int count, int **pipefd);
+void		close_unused_pipes(int **pipefd, int count);
+
+/* process_utils.c */
+void		child_process(t_cmd *cmd, t_shell_data *data);
+void		run_child_processes(t_cmd *cmd, t_shell_data *data);
+
+/* loop.c */
+void		start_minishell(t_shell_data *data);
+
+/* make_fake_cmd.c */
+t_cmd		*make_fake_cmd(void);
+
+/* parse_utils.c */
+int			ft_isquote(char c);
+int			ft_isspace(char c);
+char		handle_escape(char c);
+int			ft_isoper(char c);
+
+/* list_func.c */
+t_node		*create_node(int type, char *value);
+void		append_token(t_node **head, t_node *node);
 
 #endif
