2e5b1aa8654cb1c510f43bf566536a6655f0f911
Juyoung Kim <juyoukim@c1r20s7.42gyeongsan.kr>
2025-07-28 14:37:51 +0900

    fix: set_env_value norminette

--- srcs/env_set_unset.c
+++ srcs/env_set_unset.c
@@ -52,57 +52,64 @@ int	unset_env_value(t_list **env_list, const char *key)
 	return (1);
 }
 
-int	set_env_value(t_list **env_list, const char *key, const char *value)
+static int	update_existing_env(t_env *env, const char *value)
+{
+	char	*old_value;
+
+	old_value = env->value;
+	env->value = ft_strdup(value);
+	if (!env->value)
+	{
+		env->value = old_value;
+		return (1);
+	}
+	if (old_value)
+		free(old_value);
+	return (0);
+}
+
+static t_list	*create_new_env_node(const char *key, const char *value)
 {
 	t_env	*env;
+	t_list	*node;
+
+	env = malloc(sizeof(t_env));
+	if (!env)
+		return (NULL);
+	env->key = ft_strdup(key);
+	if (!env->key)
+		return (free(env), NULL);
+	env->value = ft_strdup(value);
+	if (!env->value)
+		return (free(env->key), free(env), NULL);
+	node = ft_lstnew(env);
+	if (!node)
+	{
+		free(env->key);
+		free(env->value);
+		free(env);
+		return (NULL);
+	}
+	return (node);
+}
+
+int	set_env_value(t_list **env_list, const char *key, const char *value)
+{
 	t_list	*cur;
-	char	*old_value;
-	t_list	*new_list_node;
+	t_env	*env;
+	t_list	*new_node;
 
 	cur = *env_list;
 	while (cur)
 	{
 		env = (t_env *)cur->content;
 		if (ft_strcmp(env->key, key) == 0)
-		{
-			old_value = env->value;
-			env->value = ft_strdup(value);
-			if (!env->value)
-			{
-				env->value = old_value;
-				return (1);
-			}
-			if (old_value)
-				free(old_value);
-			return (0);
-		}
+			return (update_existing_env(env, value));
 		cur = cur->next;
 	}
-	env = malloc(sizeof(t_env));
-	if (!env)
+	new_node = create_new_env_node(key, value);
+	if (!new_node)
 		return (1);
-	env->key = ft_strdup(key);
-	if (!env->key)
-	{
-		free(env);
-		return (1);
-	}
-	env->value = ft_strdup(value);
-	if (!env->value)
-	{
-		free(env->key);
-		free(env);
-		return (1);
-	}
-	new_list_node = ft_lstnew(env);
-	if (!new_list_node)
-	{
-		free(env->key);
-		free(env->value);
-		free(env);
-		return (1);
-	}
-	ft_lstadd_back(env_list, new_list_node);
+	ft_lstadd_back(env_list, new_node);
 	return (0);
 }
-
