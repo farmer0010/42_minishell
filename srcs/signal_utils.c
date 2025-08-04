a47e3e866f5ff607cb3cf7cd90a8d3abba91aed0
Taewon Kim <taewonki@c1r18s4.42gyeongsan.kr>
2025-08-04 15:02:52 +0900

    add : heredoc_sigint_handler func, null_event func

--- srcs/signal_utils.c
+++ srcs/signal_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juyoukim <juyoukim@student.42gyeongsa      +#+  +:+       +#+        */
+/*   By: taewonki <taewonki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 11:06:54 by juyoukim          #+#    #+#             */
-/*   Updated: 2025/07/17 11:06:56 by juyoukim         ###   ########.fr       */
+/*   Updated: 2025/08/04 14:56:43 by taewonki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,27 @@ void	sigint_handler(int signo)
 void	sigquit_handler(int signo)
 {
 	(void)signo;
+}
+
+void	heredoc_sigint_handler(int signo)
+{
+	(void)signo;
+	write(STDOUT_FILENO, "\n", 1);
+	g_exit_status = 130;
+	rl_done = 1;
+}
+
+int	null_event_hook(void)
+{
+	return (0);
+}
+
+int heredoc_event_hook(void)
+{
+	if (g_exit_status == 130)
+	{
+		rl_done = 1;
+		return (-1);
+	}
+	return 0;
 }
