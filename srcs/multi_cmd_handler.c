f491a4dcd7ed9b11a18db049f4391b626d26e1e0
Juyoung Kim <juyoukim@c1r18s3.42gyeongsan.kr>
2025-08-07 11:35:13 +0900

    multi_cmd_handler fix: g_exit_status 수정

--- srcs/multi_cmd_handler.c
+++ srcs/multi_cmd_handler.c
@@ -25,41 +25,77 @@ int	count_commands(t_cmd *cmd)
 	return (count);
 }
 
-static void	wait_all_children(int count)
+static void	wait_all_children(int count, pid_t last_pid)
 {
-	int	i;
-	int	status;
+	int		i;
+	int		status;
+	pid_t	waited_pid;
 
 	i = 0;
 	while (i < count)
 	{
-		waitpid(-1, &status, 0);
-		if (WIFEXITED(status))
-			g_exit_status = WEXITSTATUS(status);
-		else if (WIFSIGNALED(status))
-			g_exit_status = 128 + WTERMSIG(status);
+		waited_pid = wait(&status);
+		if (waited_pid == last_pid)
+		{
+			if (WIFEXITED(status))
+				g_exit_status = WEXITSTATUS(status);
+			else if (WIFSIGNALED(status))
+				g_exit_status = 128 + WTERMSIG(status);
+		}
 		i++;
 	}
 }
 
-int	handle_multiple_cmds(t_shell_data *data, t_cmd *cmd_list)
+static int	setup_pipes(t_shell_data *data, int num_cmds)
 {
-	int	num_cmds;
-
-	num_cmds = count_commands(cmd_list);
 	data->pipe_data.count = num_cmds;
 	data->pipe_data.idx = 0;
 	data->pipe_data.pipefd = create_pipes(num_cmds - 1);
-	if (!data->pipe_data.pipefd)
+	if (!data->pipe_data.pipefd && num_cmds > 1)
 	{
 		perror("minishell: failed to create pipes");
 		g_exit_status = 1;
-		return (g_exit_status);
+		return (1);
 	}
-	run_child_processes(cmd_list, data);
+	return (0);
+}
+
+static pid_t	fork_commands(t_shell_data *data, t_cmd *cmd_list)
+{
+	t_cmd	*curr;
+	pid_t	last_pid;
+
+	curr = cmd_list;
+	while (curr)
+	{
+		last_pid = fork();
+		if (last_pid == -1)
+		{
+			perror("fork");
+			return (-1);
+		}
+		else if (last_pid == 0)
+			child_process(curr, data);
+		curr = curr->next;
+		data->pipe_data.idx++;
+	}
+	return (last_pid);
+}
+
+int	handle_multiple_cmds(t_shell_data *data, t_cmd *cmd_list)
+{
+	int		num_cmds;
+	pid_t	last_pid;
+
+	num_cmds = count_commands(cmd_list);
+	if (setup_pipes(data, num_cmds))
+		return (g_exit_status);
+	last_pid = fork_commands(data, cmd_list);
+	if (last_pid == -1)
+		return (1);
 	close_unused_pipes(data->pipe_data.pipefd, num_cmds - 1);
 	free_pipes(data->pipe_data.pipefd, num_cmds - 1);
 	data->pipe_data.pipefd = NULL;
-	wait_all_children(num_cmds);
+	wait_all_children(num_cmds, last_pid);
 	return (g_exit_status);
 }
